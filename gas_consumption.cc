86cdd91b97ea636573d8ed112ff8ea60ad89b11c
ColtonYork <cy925323@ohio.edu>
2024-09-13 13:04:52 -0400

    Add files via upload

---
+++ gas_consumption.cc
@@ -0,0 +1,20 @@
+/**
+ *   @file: gas_consumption.cc
+ * @author: Colton York 
+ *   @date: Sep 8, 2024
+ *  @brief: Gas consumption project 1
+ */
+
+#include <iomanip>
+#include <iostream>
+#include <stdlib>
+using namespace std;
+
+
+int main (){
+
+
+    return 0;
+}
+
+
